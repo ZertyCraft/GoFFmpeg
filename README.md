48c6d75e791d87ae6a0ab4e96c9e4ef7cd78fca4
DIDIER Gaëtan <gaetan.didier@outlook.com>
2023-12-21 21:53:38 +0100

    Add Go module, update README, and add tests

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# GoFFmpeg
+# FFmpegCommand
+
+FFmpegCommand is a Go package that provides a simple and flexible way to interact with the `ffmpeg` command line interface. This package is designed to make it easy to build and execute `ffmpeg` commands programmatically, with additional features like custom execution time, output handling, and debug logging.
+
+## Features
+
+- Build `ffmpeg` commands dynamically with easy-to-use methods.
+- Set custom execution time limits for commands.
+- Retrieve the standard output or error messages from `ffmpeg`.
+- Debug mode for detailed logging of command execution.
+- Methods to check command execution status and stop it if needed.
+
+## Installation
+
+To use the FFmpegCommand package, first install it using `go get`:
+
+```bash
+go get github.com/ZertyCraft/GoFFmpeg
+```
+
+Then, import it into your Go project:
+
+```go
+import "github.com/ZertyCraft/GoFFmpeg"
+```
+
+## Usage
+
+Here's a basic example of how to use the FFmpegCommand package:
+
+```go
+package main
+
+import (
+    "log"
+    "github.com/ZertyCraft/GoFFmpeg"
+    "time"
+)
+
+func main() {
+    ffmpegCmd := ffmpegcommand.NewFFmpegCommand().
+        SetReturnOutput(false).
+        SetExecutionTime(1 * time.Minute).
+        AddArgs("-i input.mp4", "-c:v copy", "output.mp4")
+
+    output, err := ffmpegCmd.Execute()
+    if err != nil {
+        log.Fatalf("Failed to execute ffmpeg: %v", err)
+    }
+
+    if output != "" {
+        log.Println("ffmpeg output:", output)
+    }
+}
+```
+
+## Documentation
+
+### Constructor
+
+- `NewFFmpegCommand() *FFmpegCommand`: Initializes a new FFmpeg command.
+
+### Methods
+
+- `AddArgs(args ...string) *FFmpegCommand`: Adds arguments to the ffmpeg command.
+- `SetReturnOutput(returnOutput bool) *FFmpegCommand`: Determines if the output of the command should be returned.
+- `SetExecutionTime(duration time.Duration) *FFmpegCommand`: Sets the maximum execution time for the command.
+- `Execute() (string, error)`: Executes the ffmpeg command and manages the internal context.
+- `Stop()`: Stops the ffmpeg command if it's currently running.
+- `IsRunning() bool`: Checks if the ffmpeg command is currently running.
+- `GetArgs() []string`: Returns the arguments of the ffmpeg command.
+- `GetCommand() *exec.Cmd`: Returns the ffmpeg command.
+- `GetExecutionTime() time.Duration`: Returns the maximum execution time of the command.
+- `GetReturnOutput() bool`: Returns whether the command's output should be returned.
+- `GetLastError() string`: Returns the last error message, if any.
+
+### Debug Mode
+
+Enable debug mode to receive detailed logs during command execution:
+
+```go
+ffmpegCmd.EnableDebugMode(true)
+```
---
+++ goffmpeg_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestFFmpegCommand_Execute(t *testing.T) {
+	// Create a new FFmpegCommand instance
+	ffmpegCmd := NewFFmpegCommand().
+		SetReturnOutput(false).
+		SetExecutionTime(1*time.Minute).
+		AddArgs("-y", "-i", "tests/video/test_input.mp4", "-c:v", "copy", "tests/video/test_output.mp4").
+		EnableDebugMode(true)
+
+	// Execute the command
+	output, err := ffmpegCmd.Execute()
+
+	// Check if there was an error
+	if err != nil {
+		t.Fatalf("Failed to execute ffmpeg command: %v\n%s", err, ffmpegCmd.GetLastError())
+	}
+
+	// Check if the output is empty
+	if output != "" {
+		t.Errorf("Unexpected output from ffmpeg command: %s", output)
+	}
+}
+
+func TestFFmpegCommand_Stop(t *testing.T) {
+	// Create a new FFmpegCommand instance
+	ffmpegCmd := NewFFmpegCommand().
+		SetReturnOutput(false).
+		SetExecutionTime(1*time.Minute).
+		AddArgs("-y", "-i", "tests/video/test_input.mp4", "-c:v", "copy", "tests/video/test_output.mp4")
+
+	// Start executing the command in a separate goroutine
+	go func() {
+		_, _ = ffmpegCmd.Execute()
+	}()
+
+	// Stop the command
+	ffmpegCmd.Stop()
+
+	// Check if the command was stopped correctly
+	if ffmpegCmd.IsRunning() {
+		t.Errorf("Failed to stop the command")
+	}
+}
+
+func TestFFmpegCommand_AddArgs(t *testing.T) {
+	// Create a new FFmpegCommand instance
+	ffmpegCmd := NewFFmpegCommand().
+		SetReturnOutput(false).
+		SetExecutionTime(1 * time.Minute)
+
+	// Add arguments to the command
+	ffmpegCmd.AddArgs("-i", "tests/video/test_input.mp4", "-c:v", "copy", "tests/video/test_output.mp4")
+
+	// Check if the arguments were added correctly
+	args := ffmpegCmd.GetArgs()
+	expectedArgs := []string{"ffmpeg", "-i", "tests/video/test_input.mp4", "-c:v", "copy", "tests/video/test_output.mp4"}
+	if !reflect.DeepEqual(args, expectedArgs) {
+		t.Errorf("Unexpected arguments. Got %v, want %v", args, expectedArgs)
+	}
+}
+
+func TestFFmpegCommand_SetReturnOutput(t *testing.T) {
+	// Create a new FFmpegCommand instance
+	ffmpegCmd := NewFFmpegCommand().
+		SetExecutionTime(1*time.Minute).
+		AddArgs("-y", "-i", "tests/video/test_input.mp4", "-c:v", "copy", "tests/video/test_output.mp4")
+
+	// Set return output to true
+	ffmpegCmd.SetReturnOutput(true)
+
+	// Check if the return output flag was set correctly
+	if !ffmpegCmd.GetReturnOutput() {
+		t.Errorf("Failed to set return output flag correctly")
+	}
+}
+
+func TestFFmpegCommand_SetExecutionTime(t *testing.T) {
+	// Create a new FFmpegCommand instance
+	ffmpegCmd := NewFFmpegCommand().
+		SetReturnOutput(false).
+		AddArgs("-y", "-i", "tests/video/test_input.mp4", "-c:v", "copy", "tests/video/test_output.mp4")
+
+	// Set execution time to 2 minutes
+	ffmpegCmd.SetExecutionTime(2 * time.Minute)
+
+	// Check if the execution time was set correctly
+	if ffmpegCmd.GetExecutionTime() != 2*time.Minute {
+		t.Errorf("Failed to set execution time correctly")
+	}
+}
